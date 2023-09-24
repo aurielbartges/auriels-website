1f5cbb9ed916afbef4d941ffc6a64b550be7817d
Auriel Bartges <auriel@socialjetmedia.com>
2023-09-24 14:24:16 -0700

    Returned to earlier version

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "Biography"
+h2.textContent = "Who is She?"
 document.querySelector("body").appendChild(h2);
-const p = document.createElement("p");
-p.textContent = "Auriel is currently learning and further developing her software engineering skillset.";
-document.querySelector("body").appendChild(p);
-p.style.color = "white";
-const button = document.createElement("button");
-button.textContent = "Click to hide";
-document.querySelector("body").appendChild(button);
-
-let isVisible = true;
-
-button.addEventListener("click", function () {
-  if (isVisible) {
-    p.style.display = "none";
-    button.textContent = "Click to view";
-  } else {
-    p.style.display = "block";
-    button.textContent = "Click to hide";
-  }
-  isVisible = !isVisible;
-});
