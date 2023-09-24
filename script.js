8d9c2100daa1a4d0267a1285b5d07304b6e19fc1
Auriel Bartges <auriel@socialjetmedia.com>
2023-09-24 13:59:57 -0700

    Add JavaScript toggle and edit H1 on HTML

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript"
+h2.textContent = "Biography"
 document.querySelector("body").appendChild(h2);
 const p = document.createElement("p");
-p.textContent = "This is another JavaScript test";
+p.textContent = "Auriel is currently learning and further developing her software engineering skillset.";
 document.querySelector("body").appendChild(p);
 p.style.color = "white";
+const button = document.createElement("button");
+button.textContent = "Click to hide";
+document.querySelector("body").appendChild(button);
+
+let isVisible = true;
+
+button.addEventListener("click", function () {
+  if (isVisible) {
+    p.style.display = "none";
+    button.textContent = "Click to view";
+  } else {
+    p.style.display = "block";
+    button.textContent = "Click to hide";
+  }
+  isVisible = !isVisible;
+});
