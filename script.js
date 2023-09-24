1b991db6a4407aec91a5a32b589747fb6313583b
Auriel Bartges <auriel@socialjetmedia.com>
2023-09-24 13:32:47 -0700

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,7 @@
+const h2 = document.createElement("h2");
+h2.textContent = "This content added by JavaScript"
+document.querySelector("body").appendChild(h2);
+const p = document.createElement("p");
+p.textContent = "This is another JavaScript test";
+document.querySelector("body").appendChild(p);
+p.style.color = "white";
